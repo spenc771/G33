ba7f1280b4d069ee9cd849dc5f14792c76eb21a0
spenc771 <150295342+spenc771@users.noreply.github.com>
2025-04-28 15:35:40 -0500

    Update BOOKS.sql

--- BOOKS.sql
+++ BOOKS.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE books (
+  book_ID INT PRIMARY KEY,
+  title VARCHAR(100),
+  price DECIMAL(10,2),
+  publication_Year YEAR,
+  stock_Quantity INT,
+  genre VARCHAR(100),
+  bemployee_ID INT,
+  FOREIGN KEY (bemployee_ID) REFERENCES employees(employee_ID)
+);
