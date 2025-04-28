951d122a294e97e8db46cd12a0f365d37566bc28
spenc771 <150295342+spenc771@users.noreply.github.com>
2025-04-28 15:38:16 -0500

    Update BOOKS_ORDERED.sql

--- BOOKS_ORDERED.sql
+++ BOOKS_ORDERED.sql
@@ -1,3 +1,7 @@
 CREATE TABLE books_ordered (
-
+  Bbook_ID INT,
+  Oorder_ID INT,
+  PRIMARY KEY (Bbook_ID, Oorder_ID),
+  FOREIGN KEY (Bbook_ID) REFERENCES books(book_ID),
+  FOREIGN KEY (Oorder_ID) REFERENCES orders(order_ID)
 );
