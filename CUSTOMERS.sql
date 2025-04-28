049ea62f0e5c218e5de050622227f585940bc6d0
spenc771 <150295342+spenc771@users.noreply.github.com>
2025-04-28 14:54:39 -0500

    Update CUSTOMERS.sql

--- CUSTOMERS.sql
+++ CUSTOMERS.sql
@@ -1,1 +1,11 @@
-CREATE TABLE customers;
+CREATE TABLE customers (
+  Cust_ID INT PRIMARY KEY,
+  first_Name VARCHAR(50) NOT NULL,
+  last_Name VARCHAR(50) NOT NULL,
+  email_Address VARCHAR(100),
+  phone_Number VARCHAR(20),
+  street VARCHAR(100),
+  city VARCHAR(50),
+  state VARCHAR(50),
+  zip_code VARCHAR(10)
+);
